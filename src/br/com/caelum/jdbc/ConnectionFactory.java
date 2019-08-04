6ec38bdfd4983d66f561bea2d353c9284bc81460
JuniorFarias <wellington.junior@g4flex.com.br>
2019-08-04 10:00:18 -0300

    Alteração de conexão

--- src/br/com/caelum/jdbc/ConnectionFactory.java
+++ src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-	
-	public Connection getConnection() {
+	public Connection getConnection() throws ClassNotFoundException {
 		try {
+			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(
 					"jdbc:mysql://127.0.0.1:3306/fj21", "root", "root");
 		} catch (SQLException e) {
--- src/br/com/caelum/jdbc/teste/TestaConexao.java
+++ src/br/com/caelum/jdbc/teste/TestaConexao.java
@@ -7,7 +7,7 @@ import br.com.caelum.jdbc.ConnectionFactory;
 
 public class TestaConexao {
 	
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		Connection connection = new ConnectionFactory().getConnection();
 		System.out.println("Conexão aberta");
 		connection.close();
